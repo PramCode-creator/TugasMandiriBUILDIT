d98dafdd96067643d69b4f4c9ea59cc153d8aced
Pramudya Reanna Gupta <guptareanna@gmail.com>
2026-09-15 23:04:49 +0800

    Update Soal_2.c
    Implement a program to equalize three integers by decrementing the largest and incrementing the smallest until they are equal.

--- Soal_2.c
+++ Soal_2.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
 
+int main() {
+    int a, b, c, ronde = 0;
+    if (scanf("%d %d %d", &a, &b, &c) != 3) return 0;
+
+    while (a != b && b != c && a != c) {
+        if (a > b && a > c) {
+            a--;
+            if (b < c) b++; else c++;
+        } else if (b > a && b > c) {
+            b--;
+            if (a < c) a++; else c++;
+        } else {
+            c--;
+            if (a < b) a++; else b++;
+        }
+        ronde++;
+    }
+
+    printf("%d\n", ronde);
+    
+    
+    return 0;
+}
